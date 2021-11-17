84b89b1e42c5c7b6eb6a728c438033b2f09ac0e8
Tanel Tensing <tanel.tensing@nortal.com>
2021-11-17 20:18:02 +0200

    create the assignment

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # phone-numbers-interview
+
+* The goal would be to implement features in PhoneNumber class so that tests pass.
+
+* You are not expected to finish this test, no one has. This is about seeing how you approach and solve the challenge.
+* You can use anything you'd normally use during this exercise (e.g. google, ask questions, the IDE...etc).
+* We want to know your thoughts so the more you talk us through what you are doing and thinking, the better.
+* Modify whatever needed – no code is sacred.
+* Please feel free to ask questions. I may or may not be able to answer them.
---
+++ src/test/PhoneNumberTest.java
@@ -0,0 +1,58 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class PhoneNumberTest {
+    @Test
+    public void test1() {
+
+        PhoneNumber ph = new PhoneNumber("+1(858)775-2868");
+
+        assertEquals("+1(858)775-2868", ph.getOriginalText());
+        assertEquals("+18587752868", ph.getStrippedNumber());
+        assertEquals("(858)775-2868", ph.getValueAsNorthAmerican());
+        assertEquals("+1.858.775.2868", ph.getValueAsInternational());
+    }
+
+    @Test
+    public void test2() {
+        PhoneNumber ph = new PhoneNumber("+1(858)775-2868x123");
+
+        assertEquals("+1(858)775-2868x123", ph.getOriginalText());
+        assertEquals("+18587752868x123", ph.getStrippedNumber());
+        assertEquals("(858)775-2868x123", ph.getValueAsNorthAmerican());
+        assertEquals("+1(858)775-2868x123", ph.getValueAsInternational());
+    }
+
+    @Test
+    public void test3() {
+        PhoneNumber ph = new PhoneNumber("+598.123.4567x858");
+
+        assertEquals("+598.123.4567x858", ph.getOriginalText());
+        assertEquals("+5981234567x858", ph.getStrippedNumber());
+        assertNull(ph.getValueAsNorthAmerican());
+        assertEquals("+598.123.456.7x858", ph.getValueAsInternational());
+    }
+
+    @Test
+    public void test4() {
+        PhoneNumber ph = new PhoneNumber("+27 1234 5678 ext 4");
+
+        assertEquals("+27 1234 5678 ext 4", ph.getOriginalText());
+        assertEquals("+2712345678x4", ph.getStrippedNumber());
+        assertNull(ph.getValueAsNorthAmerican());
+        assertEquals("+27 1234 5678 ext 4", ph.getValueAsInternational());
+    }
+
+    @Test
+    public void test5() {
+        PhoneNumber ph = new PhoneNumber("858-775-2868");
+
+        assertEquals("858-775-2868", ph.getOriginalText());
+        assertEquals("+18587752868", ph.getStrippedNumber());
+        assertEquals("(858)775-2868", ph.getValueAsNorthAmerican());
+        assertEquals("+1.858.775.2868", ph.getValueAsInternational());
+    }
+}
